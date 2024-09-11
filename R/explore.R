c0e85ae0768930cf6a91b94a97e4414137a00670
Tim Bender <bendertj@gmail.com>
2024-09-11 16:09:51 -0400

    commit

--- R/explore.R
+++ R/explore.R
@@ -4,9 +4,10 @@ library(dplyr)
 rm(list=ls());cat('\f');gc()
 # snapshot()
 
-df_tr2023 <- data.frame(tr = c(.1,.12,.22,
-                               .24,.32,.35,
-                               .37), 
+df_tr2023 <- data.frame(lvl = LETTERS[1:7], 
+                        tr    = c(.1,.12,.22,
+                                  .24,.32,.35,
+                                  .37), 
                         lower = c(0,11001,44726,95376,
                                   182101,231251,578126),
                         upper = c(11000,44725,95375,
@@ -14,3 +15,18 @@ df_tr2023 <- data.frame(tr = c(.1,.12,.22,
                                   999999999))
 
 df_tr2023
+
+taxbrax <- function(inc = 1234567){
+  out <- df_tr2023
+  out$taxed <- 0
+  out[out$upper <= inc,]$taxed <- (out$upper[out$upper <= inc] - 
+                                     out$lower[out$upper <= inc]) * 
+    out$tr[out$upper <= inc]
+  
+  out[min(which(!out$upper <= inc)),]$taxed <- (inc - 
+                                                  out[min(which(!out$upper <= inc)),]$lower) *
+    out[min(which(!out$upper <= inc)),]$tr
+  return(out)
+}
+
+sum(taxbrax(150000+39000)$taxed)*0.26
---
+++ R/math_problems.R
@@ -0,0 +1,9 @@
+# math puzzles----
+
+library(dplyr)
+library(ggplot2)
+library(renv)
+
+renv::status()
+renv::snapshot()
+#renv::restore()
