99fce2c2241751ef71b407c5c1b164afd4e0bc32
Tim Bender <bendertj@gmail.com>
2024-10-22 21:53:49 -0400

    airports data rds

---
+++ R/msfs_flight_gen.R
@@ -0,0 +1,33 @@
+# msfs flight generator
+
+library(renv)
+library(dplyr)
+library(ggplot2)
+library(readr)
+
+status()
+snapshot()
+
+# web resources----
+
+# hand-crafted airports by software edition:
+# https://flight.fandom.com/wiki/Microsoft_Flight_Simulator_(2020)/List_of_hand-crafted_airports
+
+sample(c(letters,LETTERS,0:9,"!"), size = 12, replace = T) %>%
+  paste(., sep = "", collapse = "")
+
+
+airports     <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/airports.csv")
+cw_countries <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/countries.csv")
+cw_regions   <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/regions.csv")
+cw_runways   <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/runways.csv")
+cw_freq      <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/airport-frequencies.csv")
+cw_navaids   <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/navaids.csv")
+
+saveRDS(object = list(airports, 
+                      cw_countries, 
+                      cw_regions, 
+                      cw_runways, 
+                      cw_freq, 
+                      cw_navaids), 
+        file = "airport_list.rds")
