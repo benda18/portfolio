1df0a6a9e307a98330602b45ef6663c65001f4c4
Tim Bender <bendertj@gmail.com>
2024-10-22 21:58:21 -0400

    periodic

--- R/msfs_flight_gen.R
+++ R/msfs_flight_gen.R
@@ -8,40 +8,40 @@ library(readr)
 status()
 snapshot()
 
+rm(list=ls())
+
 # web resources----
 
 # hand-crafted airports by software edition:
 # https://flight.fandom.com/wiki/Microsoft_Flight_Simulator_(2020)/List_of_hand-crafted_airports
 
-sample(c(letters,LETTERS,0:9,"!"), size = 12, replace = T) %>%
-  paste(., sep = "", collapse = "")
 
-
-airports     <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/airports.csv")
-cw_countries <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/countries.csv")
-cw_regions   <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/regions.csv")
-cw_runways   <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/runways.csv")
-cw_freq      <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/airport-frequencies.csv")
-cw_navaids   <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/navaids.csv")
-
-apl <- list(airports,
-            cw_countries,
-            cw_regions,
-            cw_runways,
-            cw_freq,
-            cw_navaids)
-
-names(apl) <- c("airports", 
-                "cw_countries",
-                "cw_regions",
-                "cw_runways",
-                "cw_freq",
-                "cw_navaids")
-
-saveRDS(object = apl,
-        file = "airport_list.rds")
+# airports     <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/airports.csv")
+# cw_countries <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/countries.csv")
+# cw_regions   <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/regions.csv")
+# cw_runways   <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/runways.csv")
+# cw_freq      <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/airport-frequencies.csv")
+# cw_navaids   <- read_csv("https://raw.githubusercontent.com/davidmegginson/ourairports-data/refs/heads/main/navaids.csv")
+# 
+# apl <- list(airports,
+#             cw_countries,
+#             cw_regions,
+#             cw_runways,
+#             cw_freq,
+#             cw_navaids)
+# 
+# names(apl) <- c("airports", 
+#                 "cw_countries",
+#                 "cw_regions",
+#                 "cw_runways",
+#                 "cw_freq",
+#                 "cw_navaids")
+# 
+# saveRDS(object = apl,
+#         file = "airport_list.rds")
 
 
 airportlist <- base::readRDS(file = "airport_list.rds")
 
-names(airportlist) <- "foo"
+names(airportlist)
+airportlist$airports
