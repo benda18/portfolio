8fd36e087852f4ca82799b1a06137803ab2aa275
Tim Bender <bendertj@gmail.com>
2025-01-29 15:38:33 -0500

    discogs

--- R/discogs.R
+++ R/discogs.R
@@ -1,6 +1,13 @@
 library(renv)
 library(dplyr)
+library(jsonlite)
+# library(devtools)
+# devtools::install_github('ewenme/discogger')
+library(discogger)
 
 renv::snapshot()
 renv::status()
 # https://colinfay.me/data-vinyles-discogs-r/
+
+collection_complete <- jsonlite::fromJSON(txt = "http://colinfay.me/data/collection_complete.json", 
+                                          simplifyDataFrame = TRUE)
