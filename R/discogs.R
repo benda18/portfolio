56213e79b96148c549443ce030e197658b72c04a
Tim Bender <bendertj@gmail.com>
2025-01-29 16:06:48 -0500

    discogs

--- R/discogs.R
+++ R/discogs.R
@@ -11,3 +11,17 @@ renv::status()
 # https://github.com/ewenme/discogger
 
 # DISCOGS_API_TOKEN <- "foo"
+
+
+mycol <- discogger::discogs_user_collection(user_name = "bendertj", 
+                                    access_token = "foo")
+
+mycol.df <- NULL
+for(i in 1:length(mycol$content)){
+  mycol.df <- rbind(mycol.df, 
+                    data.frame(album_title = mycol$content[[i]]$basic_information$title, 
+                               master_id   = mycol$content[[i]]$basic_information$master_id))
+}
+
+mycol.df
+
