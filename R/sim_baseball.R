c38eed2fada069c90c27ee7fb016ed786d215894
Tim Bender <bendertj@gmail.com>
2025-02-10 15:19:11 -0500

    baseball game commit

--- R/sim_baseball.R
+++ R/sim_baseball.R
@@ -13,43 +13,166 @@ rm(list=ls());cat('\f')
 # https://batflipsandnerds.com/2018/07/23/the-anatomy-of-an-at-bat/
 
 # functions----
-the.count <- function(){
-  
-}
-
-# THE GAME----
-
-# THE INNING----
-
-# AT BAT----
-pitch_outcome <- function(){
+at.bat_outcome <- function(){
   choices <- list(choices = c("B", "S", "HBP", "foul", "1B", "2B", "3B", "HR"), 
                   probs   = c(39.4,31.8, 0.24,   11.3, 2.7, 3.8, 4.7, 6.1))
   
-  
-  
-  n.lo <- 100-sum(choices$probs)
-  
-  probs <- c(b1 = .877, b2 = 1.232, 
-             b3 = 1.552, hr = 1.98)
-  
-  probs2 <- (probs / sum(probs)) 
-  
-  (round(probs2*n.lo, digits = 1)) %>% sum
-  
-  probs3 <- round(probs2 * n.lo,1)
-  
   outcome <- sample(x = choices$choices, size = 1, prob = choices$probs)
+  
+  return(outcome)
+  
+}
+the.count <- function(atb, pB = 0, pS = 0, pO = 0, 
+                      reset.count = F, print.input = F){
+  if(print.input){
+    print(atb)
+  }
+  # if HBP
+  if(atb == "HBP"){
+    # reset balls and strikes
+    pB <- 0; pS <- 0
+  }
+  # if foul 0,1,2
+  if(atb == "foul" & pS %in% c(0,1)){
+    pS <- pS + 1
+  }
+  # if foul 3+
+  if(atb == "foul" & pS >= 2){
+    pS <- pS
+  }
+  # if 1b, 2b, 3b, 4b
+  if(atb %in% c("1B", "2B", "3B", "HR")){
+    # reset balls and strikes
+    pB <- 0; pS <- 0
+  }
+  # if S 0,1,2
+  if(atb == "S" & pS %in% c(0,1,2)){
+    pS <- pS + 1
+  }
+  # if S 3
+  if(atb == "S" & pS >2){
+    pS <- 3
+  }
+  # if B 0,1,2,3
+  if(atb == "B"){
+    pB <- pB + 1
+  }
+  # # if B 4
+  if(pB == 4){
+    # pB <- 0
+    # pS <- 0
+    pO <- pO
+  }
+  
+  # if out
+  if(pS == 3){
+    pO <- pO + 1
+    # pS <- 0
+    # pB <- 0
+  }
+  
+  # reset count
+  if(reset.count){
+    pS = 0; pB = 0
+  }
+  # # reset inning
+  # if(reset.inning){
+  #   
+  # }
+  out <- c(pB = pB,
+           pS = pS, 
+           pO = pO)
+  return(out)
+}
+
+
+# THE GAME----
+# setup
+pb  <- 0
+ps  <- 0
+po  <- 0
+pi  <- 1
+pit <- T
+var_reset.count <- F
+
+# THE INNING----
+n.error <- 0
+
+while(po < 3 ){
+  # error handling
+  n.error <- n.error + 1
+  stopifnot(n.error < 1000)
+  
+  # AT BAT----
+  abo <- at.bat_outcome()
+  temp.count <- the.count(atb = abo, 
+                          pB  = pb, 
+                          pS  = ps, 
+                          pO  = po, 
+                          reset.count = F)
+  # temp
+  # was there just an out???
+  if(po != temp.count[["pO"]]){
+    po <- po + 1
+    print("-------out----------")
+    # reset count
+    var_reset.count <- T
+  }else{
+    # don't reset count
+    var_reset.count <- F
+  }
+  # /temp
+  
+  # # was it the third out? 
+  # if(po == 3){
+  #   print("------INNING OVER-----")
+  #   po <- 0
+  # }  
+  
+  if(! var_reset.count){
+    pb <- temp.count[["pB"]]
+    ps <- temp.count[["pS"]]
+    po <- temp.count[["pO"]]
+  }else{
+    pb <- 0
+    ps <- 0
+    # # was it the third out?
+    # if(po == 3){
+    #   print("------INNING OVER-----")
+    #   po <- 0
+    # }
+  }
+  
+  
+  print(unname(temp.count))
+  # was it the third out?
+  if(po == 3){
+    print("------INNING OVER-----")
+    #po <- 0 # this should be handled by the main loop
+  }
+  
+  
+  # when the batter advances (1b,2b,3b,hr,hbp,b&pb==4)
+  if(abo %in% c("1B", "2B", "3B", "HR", "HBP") | 
+     (abo == "B" & pb == 4 )){
+    print('batter advances')
+    #print(abo)
+    #print("---------------")
+  }
+  # when the batter gets out (ps == 3)
+  # print(as.character(ps));Sys.sleep(0.25)
+  if(ps == 3){
+    #print(c(pb,ps,po))
+    print("strikeout")
+    #print("---------------")
+  }
+  
+  #rm(abo, temp.count)
   
 }
 
 
-
-
-the.inning_loop <- function(){
-  
-}  
-
+# at
 
 
 # older----
